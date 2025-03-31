18270915934f2271ddc894bdd46529fe95269c39
chris <chris@tensor-link.com>
2025-03-31 05:08:28 +0000

    twk

--- temporal/modules/losses.py
+++ temporal/modules/losses.py
@@ -42,7 +42,7 @@ class TimeSeriesLoss(BaseLoss):
             "mse": nn.MSELoss(reduction="none"),
             "mae": nn.L1Loss(reduction="none"),
             "rmse": lambda preds, labels: torch.sqrt(nn.MSELoss(reduction="none")(preds, labels)),
-            "quantile": self.quantile_loss,
+            "quantile": QuantileLoss(),
             "mq": MQLoss(),  # Placeholder for CRPS implementation
         }
 
