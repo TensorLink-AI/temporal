8cebcdd222fb5d48040030148e3430a45d27e261
chris <chris@tensor-link.com>
2025-04-01 23:40:25 +0000

    debug

--- temporal/modules/embedding.py
+++ temporal/modules/embedding.py
@@ -53,7 +53,7 @@ class TimeSeriesValueEmbedding(BaseEmbedding):
         self.value_projection = nn.Linear(feature_size, d_model, bias=False)
 
     def forward(self, x):
-        #print("x.shape:", x.shape)
+        print("x.shape:", x.shape)
 
         return self.value_projection(x)
 
