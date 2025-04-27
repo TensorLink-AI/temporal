247dd72bee15eddda222a65adbd48aecae7caea4
chris <chris@tensor-link.com>
2025-04-27 06:16:04 +0000

    attn

--- temporal/modules/attentions/__init__.py
+++ temporal/modules/attentions/__init__.py
@@ -1,15 +1,14 @@
 # ---- Attention kernels --------------------------------------------
-from .common_attentions import FullAttention, LocalAttention, FlashAttention
-from .diff_attention    import DifferenceAttention
+from .base_attentions import FullAttention,  FlashAttention
+from .diff_attention    import DiffWistAttentionWithCache
 from .hybrid_attention  import HybridAttention
 from .time_attention    import TimeFeatureAttention
 from .timer_attention   import TimerAttention
 
 __all__ = [
     "FullAttention",
-    "LocalAttention",
     "FlashAttention",
-    "DifferenceAttention",
+    "DiffWistAttentionWithCache",
     "HybridAttention",
     "TimeFeatureAttention",
     "TimerAttention",
