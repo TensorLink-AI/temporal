6c7543cd46c95f6f1ae66aa509d874fd07d19678
chris <chris@tensor-link.com>
2025-04-28 19:39:35 +0000

    k

--- temporal/modules/encoders/transformer_encoder_layer.py
+++ temporal/modules/encoders/transformer_encoder_layer.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from temporal.models.builder import ModuleBuilder
+from temporal.models.module_builder_helper import ModuleBuilder
 
 from temporal.registry.core import register_module
 
