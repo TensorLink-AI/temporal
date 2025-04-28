c7feaced2a86f13929889c575522b32fa4666fca
chris <chris@tensor-link.com>
2025-04-28 10:37:35 +0000

    rel path

--- temporal/modules/decoders/__init__.py
+++ temporal/modules/decoders/__init__.py
@@ -1,6 +1,6 @@
 # ---- Encoder stacks -----------------------------------------------
 from .decoders                  import TimeSeriesTransformerDecoder
-from .transformer_decoder_layer  import TimeSeriesTransformerDecoderLayer
+from .base_decoder_layer  import TimeSeriesTransformerDecoderLayer
 
 __all__ = [
     "TimeSeriesTransformerDecoder",
