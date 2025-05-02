4afee04f5bd0a0966d1ff2f119ca42d381fea93b
chris <chris@tensor-link.com>
2025-05-02 06:57:26 +0000

    init

--- temporal/modules/embedders/__init__.py
+++ temporal/modules/embedders/__init__.py
@@ -2,8 +2,18 @@
 from .embedding                   import TimeSeriesValueEmbedding
 # from .embedding                   import PositionalEmbedding # Commented out - was removed/commented in embedding.py
 from .embedding                   import SinusoidalPositionalEmbedding
-from .embedding             import TimeSeriesPatchEmbedding
-from .embedding            import TimeSeriesGlobalEmbedding
+from .embedding                   import TimeSeriesPatchEmbedding
+from .embedding                   import TimeSeriesGlobalEmbedding
+from .embedding                   import RotaryPositionalEmbedding
+from .embedding                   import LearnedAbsolutePositionalEmbedding
+from .embedding                   import ShawRelativePositionalBias
+from .embedding                   import FourierFeatureEmbedding
+from .embedding                   import Time2VecEmbedding
+from .embedding                   import ALiBiPositionalBias
+from .embedding                   import BucketedRelativeBias
+from .embedding                   import ConvolutionalPositionalEmbedding
+from .embedding                   import TimeDeltaEmbedding
+from .embedding                   import StackedPositionalEmbedding # Add the new stacked embedding
 
 __all__ = [
     "TimeSeriesValueEmbedding",
@@ -11,4 +21,14 @@ __all__ = [
     "SinusoidalPositionalEmbedding",
     "TimeSeriesPatchEmbedding",
     "TimeSeriesGlobalEmbedding",
+    "RotaryPositionalEmbedding",
+    "LearnedAbsolutePositionalEmbedding",
+    "ShawRelativePositionalBias",
+    "FourierFeatureEmbedding",
+    "Time2VecEmbedding",
+    "ALiBiPositionalBias",
+    "BucketedRelativeBias",
+    "ConvolutionalPositionalEmbedding",
+    "TimeDeltaEmbedding",
+    "StackedPositionalEmbedding", # Add the new stacked embedding class name
 ]
