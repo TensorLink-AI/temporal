c0aa040d3f3b03fb17b12a5a782a93e809cde40b
chris <chris@tensor-link.com>
2025-05-02 01:59:14 +0000

    d

--- temporal/modules/embedders/__init__.py
+++ temporal/modules/embedders/__init__.py
@@ -1,13 +1,13 @@
 # ---- Embedding modules --------------------------------------------
 from .embedding                   import TimeSeriesValueEmbedding
-from .embedding                   import PositionalEmbedding
+# from .embedding                   import PositionalEmbedding # Commented out - was removed/commented in embedding.py
 from .embedding                   import SinusoidalPositionalEmbedding
 from .embedding             import TimeSeriesPatchEmbedding
 from .embedding            import TimeSeriesGlobalEmbedding
 
 __all__ = [
     "TimeSeriesValueEmbedding",
-    "PositionalEmbedding",
+    # "PositionalEmbedding", # Commented out
     "SinusoidalPositionalEmbedding",
     "TimeSeriesPatchEmbedding",
     "TimeSeriesGlobalEmbedding",
