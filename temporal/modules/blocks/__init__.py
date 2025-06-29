2d1f5e7798522e62e321203e6e53cbe24c4b02bf
chris <chris@tensor-link.com>
2025-06-29 01:16:21 +0000

    d

--- temporal/modules/blocks/__init__.py
+++ temporal/modules/blocks/__init__.py
@@ -4,8 +4,7 @@
 # Import all block modules here so that the @register_block decorator is triggered
 # and the block types are available in the central registry.
 
-from . import block_hierarchical
 from . import block_multiscale
 from . import effitime_block
-from . import block_adaptive # Add this line to register the new block
 from . import block_s4 # Add this line to register the new block
+from . import patch_transform_block
