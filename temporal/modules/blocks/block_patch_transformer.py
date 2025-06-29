c57d5b573198bd8b06dc939a372b5214225472ba
chris <chris@tensor-link.com>
2025-06-29 11:21:00 +0000

    sd

--- temporal/modules/blocks/block_patch_transformer.py
+++ temporal/modules/blocks/block_patch_transformer.py
@@ -9,6 +9,7 @@ from temporal.registry.core import register_module
 from temporal.models.module_builder_helper import ModuleBuilder
 from temporal.models.block_builder import BlockBuilder
 from temporal.configs.transformer_config import TransformerBlockConfig
+from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions
 
 @register_module("block", "patch_transform_block")
 class PatchTransformBlock(nn.Module):
