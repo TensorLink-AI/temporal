3bb3e3aee1d9c6b0dbe7cc0f34e40a82f14e9264
chris <chris@tensor-link.com>
2025-07-07 10:49:25 +0000

    gd

--- temporal/models/transformer_model.py
+++ temporal/models/transformer_model.py
@@ -262,7 +262,9 @@ class TransformerTemporalModel(AutoregressiveDispatchMixin,AutoregressivePatchMi
             patch_preds = self.output_patch_reconstructor(input_to_heads)
             # reshape into [B, T_tokens * output_patch_size, feature_size]
             B, Ttok, _ = patch_preds.shape
+            f_sz  = self.config.feature_size
             op = self.preprocessor.value_embedding.output_patch_size
+            
             input_to_heads = patch_preds.view(B, Ttok * op, f_sz)
 
         # Step 3: Align head input with targets for loss calculation if needed.
