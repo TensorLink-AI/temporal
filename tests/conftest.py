77c5342cf8774018a2124d12a2212d4988530d82
chris <chris@tensor-link.com>
2025-08-15 00:45:06 +0000

    t

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 # tests/conftest.py
 
+import sys
+import os
 import pytest
 
-# You can add pytest configuration options here if needed.
+# Add the project root to the python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 @pytest.fixture
 def sample_transformer_config():
-    from temporal.configs.transformer_config import TransformerConfig
-    return TransformerConfig()
+    from temporal.configs.transformer_model_config import TransformerTimeSeriesConfig
+    return TransformerTimeSeriesConfig()
