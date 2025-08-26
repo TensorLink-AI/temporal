e8b1c3f34a5dc6defca997281b562cdb5adfb42c
chris <chris@tensor-link.com>
2025-08-26 11:18:02 +0000

    sphinx

---
+++ docs/changelog.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## Unreleased
+
+- Initial release.
+
+## vX.Y.Z (YYYY-MM-DD)
+
+- No changes yet.
---
+++ docs/conf.py
@@ -0,0 +1,85 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+# -- Path setup --------------------------------------------------------------
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "temporal"
+copyright = "2024, Your Name"
+author = "Your Name"
+
+try:
+    from temporal import __version__
+    version = __version__
+    release = version
+except ImportError:
+    version = "0.0.0"
+    release = "0.0.0"
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "autoapi.extension",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# MyST configuration
+myst_enable_extensions = [
+    "colon_fences",
+    "deflist",
+    "fieldlist",
+    "attrs_block",
+    "attrs_inline",
+]
+
+# AutoAPI configuration
+autoapi_type = "python"
+autoapi_dirs = ["../temporal"]
+autoapi_root = "api"
+autoapi_keep_files = True
+autoapi_options = {
+    "show-inheritance-diagram": False,
+    "show-module-summary": True,
+}
+
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+try:
+    import furo
+    html_theme = "furo"
+except ImportError:
+    html_theme = "pydata_sphinx_theme"
+
+
+html_static_path = ["_static"]
